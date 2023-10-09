1c21a7e489847ce43ae388087105f291aa841c3b
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-09 18:32:04 -0500

    add plugin custom permalink

---
+++ wp-content/themes/mytheme/page-contact.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ Template Name: Contact
+ Template Post Type: post, page
+ */
+?>
+<?php get_header(); ?>
+
+
+<div id="content">
+
+
+    <section id="main-content">
+        <div class="contact-info">
+        <h4>Address</h4>
+        <p>[address...]</p>
+        <p>[phone...]</p>
+        </div>
+        <div class="contact-form">
+            contact-form shortcut
+        </div>
+    </section>
+    <section id="sidebar">
+        <?php get_sidebar(); ?>
+    </section>
+
+
+</div>
+
+<?php get_footer(); ?>
