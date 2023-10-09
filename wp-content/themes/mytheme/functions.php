ed5a5142e77dc00ef00c297dd1c1ab83bfccbdf1
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-09 14:12:30 -0500

    add css with wp_head() add js with wp_footer()

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,9 +1,27 @@
 <?php
 
 function mytheme_register_styles() {
-	wp_enqueue_style('mytheme-boostrap', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+	$version = wp_get_theme()->get('Version');
+
+	//load bootstrap first then style.css
+	wp_enqueue_style('mytheme-style', get_template_directory_uri() . "/style.css", array('mytheme-bootstrap'), $version, 'all');
+	wp_enqueue_style('mytheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+	wp_enqueue_style('mytheme-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
 
 //when wordpress run this hook also run my function
 add_action('wp_enqueue_scripts', 'mytheme_register_styles');
+
+function mytheme_register_script() {
+	$version = wp_get_theme()->get('Version');
+
+
+	wp_enqueue_script('mytheme-jquery', "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(''), "3.4.1", true);
+	wp_enqueue_script('mytheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(''), "4.4.1", true);
+	wp_enqueue_script('mytheme-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(''), "1.16.0", true);
+	wp_enqueue_script('mytheme-myscript', get_template_directory_uri() . "/assets/js/main.js", array(''), $version, true);
+}
+
+//when wordpress run this hook also run my function
+add_action('wp_enqueue_scripts', 'mytheme_register_script');
 ?>
