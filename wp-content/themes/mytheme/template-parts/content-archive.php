b70daf58998e4b40282a5cea39b6bae6dd7a1f8f
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-09 16:22:27 -0500

    show thumbnail

--- wp-content/themes/mytheme/template-parts/content-archive.php
+++ wp-content/themes/mytheme/template-parts/content-archive.php
@@ -2,7 +2,8 @@
 
 	<div class="post mb-5">
 		<div class="media">
-			<img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="images/thumbs/1.jpg" alt="image">
+
+			<img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?=the_post_thumbnail_url('thumbnail')?>" alt="image">
 			<div class="media-body">
 				<h3 class="title mb-1"><?php the_title(); ?></h3>
 				<div class="meta mb-1">
