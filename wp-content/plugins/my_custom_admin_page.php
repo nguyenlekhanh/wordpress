ad4e6beb66c893f0aa696d8b6cc0e81bdfdd249d
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-09 21:45:24 -0500

    move custom admin page to plugin

---
+++ wp-content/plugins/my_custom_admin_page.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * Plugin Name: My custom admin page
+ * Description: Adds a custom admin pages with sample styles and scripts.
+ * Version: 1.0.0
+ * Author: Artbees
+ * Author URI: http://artbees.net
+ * Text Domain: my-custom-admin-page
+*/
+
+function my_admin_menu() {
+    add_menu_page(
+        __( 'Sample page', 'my-textdomain' ),
+        __( 'Sample menu', 'my-textdomain' ),
+        'manage_options',
+        'sample-page',
+        'my_admin_page_contents',
+        'dashicons-schedule',
+        3
+    );
+}
+add_action( 'admin_menu', 'my_admin_menu' );
+
+function my_admin_page_contents() {
+    ?>
+    <h1> <?php esc_html_e( 'Welcome to my custom admin page.', 'my-plugin-textdomain' ); ?> </h1>
+    <form method="POST" action="options.php">
+    <?php
+        settings_fields( 'sample-page' );
+        do_settings_sections( 'sample-page' );
+        submit_button();
+    ?>
+    </form>
+    <?php
+}
+
+
+add_action( 'admin_init', 'my_settings_init' );
+
+function my_settings_init() {
+
+    add_settings_section(
+        'sample_page_setting_section',
+        __( 'Custom settings', 'my-textdomain' ),
+        'my_setting_section_callback_function',
+        'sample-page'
+    );
+
+	add_settings_field(
+	   'my_setting_field',
+	   __( 'My custom setting field', 'my-textdomain' ),
+	   'my_setting_markup',
+	   'sample-page',
+	   'sample_page_setting_section'
+	);
+
+    add_settings_field(
+       'my_setting_field_1',
+       __( 'My custom setting field 1', 'my-textdomain' ),
+       'my_setting_markup_1',
+       'sample-page',
+       'sample_page_setting_section'
+    );
+
+	register_setting( 'sample-page', 'my_setting_field' );
+    register_setting( 'sample-page', 'my_setting_field_1' );
+}
+
+
+function my_setting_section_callback_function() {
+    echo '<p>Intro text for our settings section</p>';
+}
+
+
+function my_setting_markup() {
+    ?>
+    <label for="my-input"><?php _e( 'My Input' ); ?></label>
+    <input type="text" id="my_setting_field" name="my_setting_field" value="<?php echo get_option( 'my_setting_field' ); ?>">
+    <?php
+}
+
+function my_setting_markup_1() {
+    ?>
+    <label for="my-input"><?php _e( 'My Input 1' ); ?></label>
+    <input type="text" id="my_setting_field_1" name="my_setting_field_1" value="<?php echo get_option( 'my_setting_field_1' ); ?>">
+    <?php
+}
--- wp-content/themes/mytheme-child/functions.php
+++ wp-content/themes/mytheme-child/functions.php
@@ -24,98 +24,98 @@ add_action('wp_enqueue_scripts', 'mythemechild_load_theme_style', 15 );
 //string $menu_title, string $capability, 
 //string $menu_slug, callable $function = '' );
 
-function my_admin_menu() {
-	add_menu_page(
-		__( 'Sample page', 'my-textdomain' ),
-		__( 'Sample menu', 'my-textdomain' ),
-		'manage_options',
-		'sample-page',
-		'my_admin_page_contents',
-		'dashicons-schedule',
-		3
-	);
-}
+// function my_admin_menu() {
+// 	add_menu_page(
+// 		__( 'Sample page', 'my-textdomain' ),
+// 		__( 'Sample menu', 'my-textdomain' ),
+// 		'manage_options',
+// 		'sample-page',
+// 		'my_admin_page_contents',
+// 		'dashicons-schedule',
+// 		3
+// 	);
+// }
 
-add_action( 'admin_menu', 'my_admin_menu' );
-
-
-function my_admin_page_contents() {
-	?>
-		<h1>
-			<?php esc_html_e( 'Welcome to my custom admin page.', 'my-plugin-textdomain' ); ?>
-		</h1>
-	<?php
-}
+// add_action( 'admin_menu', 'my_admin_menu' );
 
 
+// function my_admin_page_contents() {
+// 	?>
+// 		<h1>
+// 			<!-- <?php esc_html_e( 'Welcome to my custom admin page.', 'my-plugin-textdomain' ); ?> -->
+// 		</h1>
+// 	<?php
+// }
 
-//load stylesheet 
+
+
+//  first way to load stylesheet 
+// // function load_custom_wp_admin_style($hook) {
+// // 	// Load only on ?page=mypluginname
+// // 	if( $hook != 'toplevel_page_sample-page' ) {
+// // 		 return;
+// // 	}
+// // 	wp_enqueue_style( 'custom_wp_admin_css', plugins_url('admin-style.css', __FILE__) );
+// // }
+
+// // add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+
+// // get hook name ?page=mypluginname and fill to load_custom_wp_admin_style
+// // function load_custom_wp_admin_style( $hook ) {
+// // 	wp_die( $hook );
+// // }
+// // add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+
+
+//second way load stylesheet by Register to wordpress first
+// function register_my_plugin_styles() {
+// 	wp_register_style( 'my-plugin', plugins_url( 'my-plugin/css/plugin.css' ) );
+// }
+
+// // Register style sheet.
+
+// add_action( 'admin_enqueue_scripts', 'register_my_plugin_styles' );
+
+
 // function load_custom_wp_admin_style($hook) {
+
 // 	// Load only on ?page=mypluginname
+
 // 	if( $hook != 'toplevel_page_sample-page' ) {
-// 		 return;
+// 		return;
 // 	}
-// 	wp_enqueue_style( 'custom_wp_admin_css', plugins_url('admin-style.css', __FILE__) );
+
+// 	// Load style
+
+// 	wp_enqueue_style( 'my-plugin' );
 // }
 
 // add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 
-//get hook name ?page=mypluginname and fill to load_custom_wp_admin_style
-// function load_custom_wp_admin_style( $hook ) {
-// 	wp_die( $hook );
+
+
+
+// function register_my_plugin_scripts() {
+// 	wp_register_script( 'my-plugin', get_stylesheet_directory_uri() . '/plugin-scripts.js', array( 'jquery' ) );
 // }
-// add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
-
-
-//load stylesheet by Register to wordpress first
-function register_my_plugin_styles() {
-	wp_register_style( 'my-plugin', plugins_url( 'my-plugin/css/plugin.css' ) );
-}
-
-// Register style sheet.
-
-add_action( 'admin_enqueue_scripts', 'register_my_plugin_styles' );
-
-
-function load_custom_wp_admin_style($hook) {
-
-	// Load only on ?page=mypluginname
-
-	if( $hook != 'toplevel_page_sample-page' ) {
-		return;
-	}
-
-	// Load style
-
-	wp_enqueue_style( 'my-plugin' );
-}
-
-add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 
 
 
+// // Register style sheet.
 
-function register_my_plugin_scripts() {
-	wp_register_script( 'my-plugin', get_stylesheet_directory_uri() . '/plugin-scripts.js', array( 'jquery' ) );
-}
-
+// add_action( 'admin_enqueue_scripts', 'register_my_plugin_scripts' );
 
 
-// Register style sheet.
+// function load_custom_wp_admin_scripts($hook) {
 
-add_action( 'admin_enqueue_scripts', 'register_my_plugin_scripts' );
+// 	// Load only on ?page=mypluginname
+// 	if($hook != 'toplevel_page_mypluginname') {
+// 		return;
+// 	}
 
+// 	// Load style
+// 	wp_enqueue_script( 'my-plugin' );
 
-function load_custom_wp_admin_scripts($hook) {
+// }
 
-	// Load only on ?page=mypluginname
-	if($hook != 'toplevel_page_mypluginname') {
-		return;
-	}
-
-	// Load style
-	wp_enqueue_script( 'my-plugin' );
-
-}
-
-add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_scripts' );
+// add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_scripts' );
